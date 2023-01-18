65cf33ff3f6d525bc584fa4e068ba38f9f338e18
DevPBDias <devpbdias@gmail.com>
2023-01-18 20:30:22 -0300

    feat: implemented scrape_news function to return a dict with info

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -43,8 +43,34 @@ def scrape_next_page_link(html_content: str) -> str:
 
 
 # Requisito 4
-def scrape_news(html_content):
-    pass
+def scrape_news(html_content: str) -> dict:
+    # https://www.w3schools.com/python/ref_string_join.asp
+    # https://www.w3schools.com/python/ref_string_strip.asp
+    # https://www.w3schools.com/cssref/sel_first-of-type.php
+    try:
+        selector = Selector(html_content)
+
+        comments_number = selector.css(
+                "div.pk-share-buttons-count::text").get()
+        string = ""
+
+        info_news = {
+            "url": selector.css("link[rel=canonical]::attr(href)").get(),
+            "title": selector.css("h1.entry-title::text").get().strip(),
+            "timestamp": selector.css("li.meta-date::text").get(),
+            "writer": selector.css("a.url.fn.n::text").get(),
+            "comments_count": comments_number if comments_number else 0,
+            "summary": string.join(selector.css(
+                    "div.entry-content > p:first-of-type *::text").getall()
+                    ).strip(),
+            "tags": selector.css("section.post-tags ul li a::text").getall(),
+            "category": selector.css(
+                "a.category-style span.label::text").get(),
+        }
+
+        return info_news
+    except info_news is None:
+        return None
 
 
 # Requisito 5
