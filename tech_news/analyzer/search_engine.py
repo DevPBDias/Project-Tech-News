508a280d4e4d60928cd81a438ecfeed2915d9942
DevPBDias <devpbdias@gmail.com>
2023-01-19 10:48:21 -0300

    feat: implemented search_by_title function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,32 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    # https://www.mongodb.com/docs/manual/reference/operator/query/regex/
+    try:
+        titles = search_news({"title": {"$regex": title, "$options": "i"}})
+        info = []
+
+        for item in titles:
+            info.append((item["title"], item["url"]))
+
+        return info
+
+    except titles is None:
+        return []
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    pass
 
 
 # Requisito 8
 def search_by_tag(tag):
-    """Seu código deve vir aqui"""
+    pass
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    pass
